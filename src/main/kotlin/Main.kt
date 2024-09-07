62a15b66616a60ec1e0528d25f5ae762b1f25514
Низомов Низом Джумъабойевич <2441-22@std.pkgh.edu>
2024-09-07 18:36:27 +0300

    Pt1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,22 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val inputVal = "asdasd"
+    var result = " "
+    var countChar = 1
+    var previousChar = inputVal[0]
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    for (i in 1..inputVal.length - 1) {
+        if (inputVal[i] == previousChar) {
+            countChar++
+        } else {
+            if (countChar > 1) {
+                result = result + previousChar + countChar
+            } else {
+                result = result + previousChar
+            }
+            countChar = 1
+            val i = 0
+            previousChar = inputVal[i]
+        }
+    }
+    println(result)
 }
